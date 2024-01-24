da989b8b0f82309c176ec120f97c8a944f220af3
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2024-01-24 22:57:01 +0000

    88 - Merge Sorted Array completed!

--- src/88_MergeSortedArray/mergeSortedArray.test.ts
+++ src/88_MergeSortedArray/mergeSortedArray.test.ts
@@ -1,31 +1,31 @@
 import { merge } from "./mergeSortedArray"
 
 describe("mergeSortedArray", () => {
-    it("should merge two sorted arrays", () => {
-        // Example 1
+    it("Example 1 - should merge two sorted arrays", () => {
         let nums1 = [1, 2, 3, 0, 0, 0]
         let m = 3
         let nums2 = [2, 5, 6]
         let n = 3
         merge(nums1, m, nums2, n)
+
         expect(nums1).toEqual([1, 2, 2, 3, 5, 6])
+    })
 
-        // Example 2
-        nums1 = [1]
-        m = 1
-        nums2 = []
-        n = 0
-        merge(nums1, m, nums2, n)
-        expect(nums1).toEqual([1])
-
-        // Example 3
-        nums1 = [0]
-        m = 0
-        nums2 = [1]
-        n = 1
+    it("Example 2 - should merge array with empty array", () => {
+        let nums1 = [1]
+        let m = 1
+        let nums2 = []
+        let n = 0
         merge(nums1, m, nums2, n)
         expect(nums1).toEqual([1])
     })
 
-    // Add more test cases as needed based on constraints and edge cases
+    it("Example 3 - should merge empty array with array", () => {
+        let nums1 = [0]
+        let m = 0
+        let nums2 = [1]
+        let n = 1
+        merge(nums1, m, nums2, n)
+        expect(nums1).toEqual([1])
+    })
 })
--- src/88_MergeSortedArray/mergeSortedArray.ts
+++ src/88_MergeSortedArray/mergeSortedArray.ts
@@ -1,4 +1,32 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
-function merge(nums1: number[], m: number, nums2: number[], n: number): void {}
+export function merge(
+    nums1: number[],
+    m: number,
+    nums2: number[],
+    n: number
+): void {
+    // Remove last n chars
+    nums1.splice(m, n) // in index 3, removes 3 elements
+
+    console.log(nums1)
+    /* 
+    Note: this won't work, re-assigning the array to a new one, the reference to the initial gets lost,
+    and the changes not applied outside the merge function scope
+    
+    nums1 = [...nums1, ...nums2] -> re-assign
+    nums1 = nums1.concat(nums2) -> re-assign, porque concat nao altera arrays originais
+    */
+
+    // Add nums2 into nums2, keeping the reference of nums1 from outside merge function
+    for (let num of nums2) {
+        nums1.push(num)
+    }
+
+    console.log(nums1)
+
+    // sort asc
+    nums1.sort((a, b) => a - b)
+    console.log(nums1)
+}
