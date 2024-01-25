72f664ec95b6785053b8b2f8d678684445dfc007
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2024-01-25 11:13:09 +0000

    Remove duplicates done!

--- src/80_Medium_Remove_Duplicates_from_Sorted_Array_II/removeDuplicates.ts
+++ src/80_Medium_Remove_Duplicates_from_Sorted_Array_II/removeDuplicates.ts
@@ -1,3 +1,25 @@
 export function removeDuplicates(nums: number[]): number {
-    return 0
+    const numsCountMap: Map<number, number> = new Map()
+
+    let nextMapEntryCount: number = 0
+    let numsIndex: number = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!numsCountMap.has(nums[i])) {
+            numsCountMap.set(nums[i], 1)
+
+            nums[numsIndex] = nums[i]
+            numsIndex++
+        } else if ((numsCountMap.get(nums[i]) || 0) < 2) {
+            nextMapEntryCount = numsCountMap.get(nums[i]) || 0
+            numsCountMap.set(nums[i], nextMapEntryCount + 1)
+
+            nums[numsIndex] = nums[i]
+            numsIndex++
+        }
+    }
+    console.log(nums)
+    console.log(numsIndex)
+
+    return numsIndex
 }
