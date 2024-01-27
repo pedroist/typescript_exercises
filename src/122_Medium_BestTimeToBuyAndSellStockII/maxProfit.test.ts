f14655595a55019473076a834f443acdff3d5bbf
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2024-01-27 12:04:52 +0000

    122_Medium_BestTimeToBuyAndSellStockII done!

---
+++ src/122_Medium_BestTimeToBuyAndSellStockII/maxProfit.test.ts
@@ -0,0 +1,83 @@
+import { evalNumb, maxProfit } from "./maxProfit"
+
+describe("maxProfit", () => {
+    it("returns the maximum profit for the given prices - Example 1", () => {
+        const prices: number[] = [7, 1, 5, 3, 6, 4]
+        expect(maxProfit(prices)).toEqual(7)
+    })
+
+    it("returns the maximum profit for the given prices - Example 2", () => {
+        const prices: number[] = [1, 2, 3, 4, 5]
+        expect(maxProfit(prices)).toEqual(4)
+    })
+
+    it("returns the maximum profit for the given prices - Example 3", () => {
+        const prices: number[] = [7, 6, 4, 3, 1]
+        expect(maxProfit(prices)).toEqual(0)
+    })
+
+    it("returns 0 when prices array has only one element", () => {
+        const prices: number[] = [5]
+        expect(maxProfit(prices)).toEqual(0)
+    })
+
+    it("returns 0 when prices array has all elements in descending order", () => {
+        const prices: number[] = [5, 4, 3, 2, 1]
+        expect(maxProfit(prices)).toEqual(0)
+    })
+
+    it("returns the correct profit when there are multiple profitable transactions", () => {
+        const prices: number[] = [3, 3, 5, 0, 0, 3, 1, 4]
+        // expected buy days [1, 3, 6]
+        // expected sell days [2, 5, 7]
+        expect(maxProfit(prices)).toEqual(8)
+    })
+
+    it("returns 0 when prices array has all elements equal", () => {
+        const prices: number[] = [1, 1, 1, 1, 1]
+        expect(maxProfit(prices)).toEqual(0)
+    })
+
+    it("has zeros after last buy", () => {
+        const prices: number[] = [3, 2, 6, 5, 0, 3]
+        expect(maxProfit(prices)).toEqual(7)
+    })
+
+    it("prices end with zero", () => {
+        const prices: number[] = [1, 1, 0]
+        expect(maxProfit(prices)).toEqual(0)
+    })
+
+    it("prices start with zero", () => {
+        const prices: number[] = [0, 5, 5, 6, 2, 1, 1, 3]
+        expect(maxProfit(prices)).toEqual(8)
+        // expected buy days [0, 6]
+        // expected sell days [ 3, 7]
+    })
+})
+
+describe("test evalNumb function", () => {
+    it("should return false if input value is a negative value", () => {
+        const value: number = -1
+
+        const result: boolean | undefined = evalNumb(value)
+
+        expect(result).toBe(false)
+    })
+
+    it("should return true if input value is a positive number", () => {
+        const value: number = 1
+
+        const result: boolean | undefined = evalNumb(value)
+
+        expect(result).toBeTruthy()
+    })
+
+    it("should return undefined if input value is zero", () => {
+        const value: number = 0
+
+        const result: boolean | undefined = evalNumb(value)
+
+        expect(result).toBeUndefined()
+    })
+})
---
+++ src/122_Medium_BestTimeToBuyAndSellStockII/maxProfit.ts
@@ -0,0 +1,104 @@
+export function maxProfit(prices: number[]): number {
+    // fragment the array of length n into an array of n-1 with the differences between days
+    let differencesArray: number[] = []
+
+    let priceZeroDays: number[] = []
+
+    for (let i = 0; i < prices.length - 1; i++) {
+        differencesArray[i] = prices[i + 1] - prices[i]
+
+        if (prices[i] === 0) {
+            priceZeroDays.push(i)
+        }
+    }
+    console.log(differencesArray)
+
+    // If there is only one price, the differencesArray will be empty 0 profit gets return
+    if (differencesArray.length === 0) {
+        return 0
+    }
+
+    // for each of differences, save the indexes of the days to buy and sell
+    let isPositiveLastDifference: boolean | undefined = evalNumb(
+        differencesArray[0]
+    )
+    let currentDifferenceEval: boolean | undefined
+    let buyDays: number[] = []
+    let sellDays: number[] = []
+
+    if (isPositiveLastDifference) {
+        // if first difference is positive, buy on the day 1 (index 0)
+        buyDays.push(0)
+    }
+
+    for (let i = 1; i < differencesArray.length; i++) {
+        currentDifferenceEval = evalNumb(differencesArray[i])
+        if (currentDifferenceEval !== isPositiveLastDifference) {
+            // entering means a change of a peak
+            if (
+                currentDifferenceEval === false &&
+                sellDays.length < buyDays.length
+            ) {
+                // we should only sell if we bought before
+                // Price was going up and now will start going down - Sell
+                sellDays.push(i)
+            } else if (
+                currentDifferenceEval === true &&
+                sellDays.length === buyDays.length
+            ) {
+                // can't buy if I bought first and didn't sell, number must be even
+                // Price was going down and now will start going up - Buy
+                buyDays.push(i)
+            }
+            isPositiveLastDifference = currentDifferenceEval
+        }
+    }
+
+    if (buyDays.length > sellDays.length) {
+        // find if is any day price zero after the last buy
+        if (priceZeroDays.length > 0) {
+            let lastSellDay = priceZeroDays.find(
+                (day: number) => day > buyDays[buyDays.length - 1]
+            )
+
+            if (lastSellDay) {
+                sellDays.push(lastSellDay)
+            } else {
+                //if we bought, but we never selled, the price always kept going up, then we sell on the last day
+                sellDays.push(prices.length - 1)
+            }
+        } else {
+            //if we bought, but we never selled, the price always kept going up, then we sell on the last day
+            sellDays.push(prices.length - 1)
+        }
+    }
+
+    // calculate differences between followed days of buying and sell
+    let profitParts: number[] = []
+
+    for (let i = 0; i < sellDays.length; i++) {
+        profitParts.push(prices[sellDays[i]] - prices[buyDays[i]])
+    }
+
+    // Sum all those differences
+    if (buyDays.length === 0 && sellDays.length === 0) {
+        return 0
+    }
+
+    const totalProfit: number = profitParts.reduce(
+        (acc, currentValue) => acc + currentValue,
+        0
+    )
+
+    return totalProfit
+}
+
+export const evalNumb = (value: number): boolean | undefined => {
+    if (value > 0) {
+        return true
+    } else if (value < 0) {
+        return false
+    } else {
+        return undefined
+    }
+}
